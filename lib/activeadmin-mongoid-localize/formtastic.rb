abddcda9451d95837be56db7feb3eb9d50591143
Miguel Marinhas <miguel.marinhas@gmail.com>
2013-03-14 02:14:59 +0000

    catalonian fix for me

--- lib/activeadmin-mongoid-localize/formtastic.rb
+++ lib/activeadmin-mongoid-localize/formtastic.rb
@@ -10,6 +10,9 @@ module Formtastic
           args[:value] =  (t.nil? || t[locale.to_s].nil?) ? '' : t[locale.to_s]
           flag_code = locale.to_s.include?("-") ? locale.to_s.split("-")[1].downcase : locale.to_s
 
+          # stupid fix to be removed
+          flag_code = "catalonia" if flag_code == "ca" 
+
           label = CGI.escapeHTML(self.object.class.human_attribute_name(name)) + " #{template.image_tag "aml/flags/#{flag_code}.png", alt: locale.to_s, title: locale.to_s}"
           if args[:as] == :ckeditor
             prepend = "<h3 style='margin: 10px 0px 0px 10px;'>#{label}</h3>#{'<abbr>*</abbr>' if field.required?}".html_safe
